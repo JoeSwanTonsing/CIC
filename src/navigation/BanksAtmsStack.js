aeb96f83807cfafa85dda230ff3d91e30f4b6b96
Suan Tonsing <suanasdf@gmail.com>
2020-09-30 01:07:12 +0530

    Created BanksAtmsStack

---
+++ src/navigation/BanksAtmsStack.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
+//Screens for stack
+import BankAtmScreen from '../screens/BanksAtmsScreen';
+
+const BankAtmStack = createStackNavigator();
+
+const BankAtmStackScreen = ({navigation}) => (
+  <BankAtmStack.Navigator
+    initialRouteName="Home"
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#3AB795',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}>
+    <BankAtmStack.Screen
+      name="Home"
+      component={BankAtmScreen}
+      options={{
+        title: ' Banks & ATMs ',
+        headerLeft: () => (
+          <Icon.Button
+            name="menu"
+            backgroundColor="#3AB795"
+            color="#fff"
+            size={30}
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+          />
+        ),
+      }}
+    />
+  </BankAtmStack.Navigator>
+);
+
+export default BankAtmStackScreen;
