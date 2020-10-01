c177ddafc4b38a3510d3c054a5d93359368acd0f
Suan Tonsing <suanasdf@gmail.com>
2020-10-01 14:04:06 +0530

    Created pharmacy screen

---
+++ src/screens/healthScreens/PharmacyScreen.js
@@ -0,0 +1,87 @@
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, ScrollView, View, Text, Image} from 'react-native';
+import axios from 'axios';
+import Spinner from '../../components/Spinner';
+
+//components
+import HealthCard from '../../components/HealthCard';
+
+export default function PharmacyScreen({navigation}) {
+  const [pharmacies, setPharmacies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(
+        'https://joeswantonsing.github.io/CIC_Data/api/health/pharmacies.json',
+      )
+      .then((res) => {
+        setPharmacies(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.savStyle}>
+      <View style={styles.introSection}>
+        <Image
+          style={styles.introImage}
+          source={require('../../assets/icons/medicine.png')}
+        />
+        <View>
+          <Text style={styles.introText}>Listing All Pharmacies</Text>
+          <Text style={styles.introText}>Churachandpur District</Text>
+        </View>
+      </View>
+      <ScrollView style={styles.container}>
+        {isLoading ? (
+          <View style={styles.spinnerContainer}>
+            <Spinner size="large" color="#53599A" spinnerText="Loading" />
+          </View>
+        ) : null}
+        <View style={styles.listContainer}>
+          {pharmacies.map((pharmacy) => (
+            <HealthCard key={pharmacy.id} cardData={pharmacy} />
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = {
+  savStyle: {
+    flex: 1,
+  },
+  introSection: {
+    padding: 15,
+    justifyContent: 'center',
+    borderBottomColor: '#d1d1d1',
+    borderBottomWidth: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+  },
+  introImage: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+    marginRight: 5,
+    tintColor: '#53599A',
+  },
+  introText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 5,
+    color: '#53599A',
+  },
+  spinnerContainer: {
+    marginTop: 20,
+  },
+  listContainer: {
+    padding: 8,
+  },
+};
